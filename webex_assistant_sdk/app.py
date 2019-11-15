5470b9c8c01bfc4b11aba75fb3171ae540449f63
J.J. Jackson <jdot@cisco.com>
2019-11-15 09:55:18 -0800

    Use Assistant Responder

--- webex_assistant_sdk/app.py
+++ webex_assistant_sdk/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from mindmeld import Application
 
+from .dialogue import AssistantDialogueResponder
 from .server import create_agent_server
 
 
@@ -9,8 +10,17 @@ class AgentApplication(Application):
     An implementation of AgentApplication using AgentServer.
     """
 
-    def __init__(self, import_name, *, secret, private_key, **kwargs):
-        super().__init__(import_name, **kwargs)
+    def __init__(
+        self,
+        import_name,
+        *,
+        secret,
+        private_key,
+        responder_class=AssistantDialogueResponder,
+        **kwargs,
+    ):
+
+        super().__init__(import_name, responder_class=responder_class, **kwargs)
         self.secret = secret
         self.private_key = private_key
 
