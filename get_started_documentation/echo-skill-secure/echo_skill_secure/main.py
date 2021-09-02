f101a39fc7cd63cef43baa28334fd2e7b9fb91f8
Juan Rodriguez <angel.neb@gmail.com>
2021-09-02 14:28:51 -0500

    Fixed encryption logic in sample skill

--- get_started_documentation/echo-skill-secure/echo_skill_secure/main.py
+++ get_started_documentation/echo-skill-secure/echo_skill_secure/main.py
@@ -1,8 +1,14 @@
+import base64
 import json
 import os
 
 from aiohttp import web
-from cryptography.hazmat.primitives import hashes, hmac, serialization
+
+from cryptography.exceptions import InvalidSignature
+from cryptography.hazmat.primitives import hashes, hmac
+from cryptography.hazmat.primitives.asymmetric.padding import MGF1, OAEP
+from cryptography.hazmat.primitives.serialization import load_ssh_private_key
+
 
 DEV_MODE = os.getenv('ECHO_RUN_DEV_MODE', False)
 PRIVATE_KEY = os.getenv('ECHO_PRIVATE_KEY')
@@ -12,15 +18,20 @@ SECRET = os.getenv('ECHO_SECRET')
 routes = web.RouteTableDef()
 
 
-def decrypt_body(body: str) -> dict:
-    pass
+def decrypt(message: bytes):
+    # We expect our message to be a base64 encoded byte string of our cipher text
+    key_bytes = PRIVATE_KEY.encode('utf-8')
+    private_key = load_ssh_private_key(key_bytes, None)
+    padding = OAEP(mgf=MGF1(algorithm=hashes.SHA256()),
+                   algorithm=hashes.SHA256(), label=None)
+    return private_key.decrypt(message, padding)
 
 
-def verify_signature(signature: str, body: dict) -> bool:
-    _hmac = hmac.HMAC(json.dumps(body), hashes.SHA256())
-    _hmac.update(SECRET)
-    created_signature = _hmac.finalize().hex()
-    return created_signature == signature
+def verify_signature(message: bytes, signature: bytes):
+    secret_bytes = SECRET.encode('utf-8')
+    _hmac = hmac.HMAC(secret_bytes, hashes.SHA256())
+    _hmac.update(message)
+    _hmac.verify(signature)
 
 
 def directive(name: str, dtype: str, payload: dict = None) -> dict:
@@ -63,12 +74,38 @@ async def echo(request: web.BaseRequest) -> web.Response:
     if DEV_MODE:
         req_body = await request.json()
     else:
-        req_body = await request.read()
-        req_body = decrypt_body(req_body)
-        signature = request.headers['X-Webex-Assistant-Signature']
-        valid_signature = verify_signature(signature, req_body)
-        if not valid_signature:
-            return web.json_response({"message": "bad request"}, 400)
+        request_body: bytes = await request.json()
+
+        # Our signature and cipher bytes are expected to be base64 encoded byte strings
+        encoded_signature: str = request_body.get('signature')
+        encoded_cipher: str = request_body.get('message')
+
+        # Bail on missing signature
+        if not encoded_signature:
+            return web.json_response({"error": "Missing signature"}, 400)
+
+        # And on a missing message
+        if not encoded_cipher:
+            return web.json_response({"error": "Missing message"}, 400)
+
+        # Convert our encoded signature and body to bytes
+        encoded_cipher_bytes: bytes = encoded_cipher.encode('utf-8')
+
+        # We sign the encoded cipher text so we decode our signature, but not our cipher text yet
+        decoded_sig_bytes: bytes = base64.b64decode(encoded_signature)
+
+        try:
+            # Cryptography's verify method throws rather than returning false..thanks jerks.
+            verify_signature(encoded_cipher_bytes, decoded_sig_bytes)
+        except InvalidSignature:
+            return web.json_response({"error": "Invalid signature"}, 400)
+
+        # Now that we've verified our signature we decode our cipher to get the raw bytes
+        decoded_cipher = base64.b64decode(encoded_cipher_bytes)
+        decrypted_body = decrypt(decoded_cipher)
+
+        # The decrypted cipher text is a json string representing our MindMeld style message
+        req_body = json.loads(decrypted_body)
 
     response = handle_message(req_body)
     return web.json_response(response)
--- get_started_documentation/echo-skill-secure/echo_skill_secure_tester/main.py
+++ get_started_documentation/echo-skill-secure/echo_skill_secure_tester/main.py
@@ -1,6 +1,9 @@
+import base64
+
+import aiohttp
+import asyncio
 import json
 import os
-import requests
 
 from cryptography.hazmat.primitives import hashes, hmac, serialization
 from cryptography.hazmat.primitives.asymmetric.padding import MGF1, OAEP
@@ -9,20 +12,18 @@ PUBLIC_KEY = os.getenv('ECHO_PUBLIC_KEY')
 SECRET = os.getenv('ECHO_SECRET')
 
 
-def encrypt_payload(message: str, key: str) -> bytes:
-    msg_bytes = message.encode('utf-8')
-    key_bytes = key.encode('utf-8')
-
-    padding = OAEP(mgf=MGF1(algorithm=hashes.SHA256()), algorithm=hashes.SHA256(), label=None)
-    public_key = serialization.load_ssh_public_key(key_bytes)
-    return public_key.encrypt(msg_bytes, padding)
+def encrypt_payload(message: bytes, key: bytes) -> bytes:
+    padding = OAEP(mgf=MGF1(algorithm=hashes.SHA256()),
+                   algorithm=hashes.SHA256(), label=None)
+    public_key = serialization.load_ssh_public_key(key)
+    return public_key.encrypt(message, padding)
 
 
-def sign_message(message: bytes, secret: bytes) -> str:
-    _hmac = hmac.HMAC(message, hashes.SHA256())
-    _hmac.update(secret)
-    signature = _hmac.finalize().hex()
-    return signature
+def sign_message(message: bytes, secret: bytes) -> bytes:
+    _hmac = hmac.HMAC(secret, hashes.SHA256())
+    _hmac.update(message)
+    sig = _hmac.finalize()
+    return sig
 
 
 message_body = {
@@ -33,14 +34,23 @@ message_body = {
     "history": {}
 }
 
-encrypted_request = encrypt_payload(json.dumps(message_body), PUBLIC_KEY)
+encrypted_message = encrypt_payload(json.dumps(message_body).encode('utf-8'), PUBLIC_KEY.encode('utf-8'))
+encoded_message = base64.b64encode(encrypted_message)
 
-sig = sign_message(encrypted_request, SECRET.encode('utf-8'))
-headers = {
-    'X-Webex-Assistant-Signature': sig,
-    'Content-Type': 'application/octet-stream',
-    'Accept': 'application/json',
+sig = sign_message(encoded_message, SECRET.encode('utf-8'))
+encoded_sig = base64.b64encode(sig)
+request = {
+    'signature': encoded_sig.decode('utf-8'),
+    'message': encoded_message.decode('utf-8')
 }
 
-response = requests.post('http://0.0.0.0:8080/', headers=headers, data=encrypted_request)
-print(response.json())
+
+async def main():
+    async with aiohttp.ClientSession() as session:
+        async with session.post('http://0.0.0.0:8080/', json=request) as resp:
+            print(f'Status: {resp.status}')
+            print(f'Response: {await resp.text()}')
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
