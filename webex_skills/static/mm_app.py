df199deb63da460538382f818ce31e745f9b7dc9
Juan Rodriguez <angel.neb@gmail.com>
2021-10-26 14:06:36 -0500

    Added new handler and default

--- webex_skills/static/mm_app.py
+++ webex_skills/static/mm_app.py
@@ -5,9 +5,23 @@ from webex_skills.models.mindmeld import DialogueState
 api = MindmeldAPI()
 
 
-@api.handle(intent='greet')
+@api.handle(intent='greet', default=True)
 async def greet(current_state: DialogueState) -> DialogueState:
     text = 'Hello I am a super simple skill using NLP'
+    new_state = current_state.copy()
+
+    new_state.directives = [
+        responses.Reply(text),
+        responses.Speak(text),
+        responses.Sleep(10),
+    ]
+
+    return new_state
+
+
+@api.handle(intent='exit')
+async def goodbye(current_state: DialogueState) -> DialogueState:
+    text = 'Have a nice day!'
     new_state = current_state.copy()
 
     new_state.directives = [
