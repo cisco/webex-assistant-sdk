0e8ae758ff8ed4108d1c8e15ca459fb22ae5356d
Brady Lenz <bradylenz98@gmail.com>
2022-03-17 18:25:44 -0500

    remove dead cli command code

--- webex_assistant_skills_sdk/cli/nlp_app/commands/process.py
+++ webex_assistant_skills_sdk/cli/nlp_app/commands/process.py
@@ -5,17 +5,12 @@ import typer
 
 from webex_assistant_skills_sdk.cli.nlp_app.app import app
 from webex_assistant_skills_sdk.cli.helpers import init_mindmeld_nlp
-from webex_assistant_skills_sdk.cli.config import get_app_dir
 
 
 @app.command()
 def process(name: Optional[str] = typer.Argument(None, help="The name of the skill to send the query to.")):
     """Run a query through NLP processing"""
-    app_dir = '.'
-    if name:
-        app_dir = get_app_dir(name)
-
-    nlp = init_mindmeld_nlp(app_dir)
+    nlp = init_mindmeld_nlp()
     nlp.load()
 
     typer.echo('Enter a query below (Ctl+C to exit)')
