6820788d501acd199622bfb7eb8f43d1c73d90b0
Brady Lenz <bradylenz98@gmail.com>
2022-04-08 11:10:52 -0700

    fix: wire container with middlewares

--- webex_assistant_skills_sdk/api/container.py
+++ webex_assistant_skills_sdk/api/container.py
@@ -1,7 +1,7 @@
 from dependency_injector import containers, providers
 
 from webex_assistant_skills_sdk.container import BaseContainer
-from webex_assistant_skills_sdk.api import shared
+from webex_assistant_skills_sdk.api import middlewares, shared
 from webex_assistant_skills_sdk.api.shared.services import CryptoService
 
 
@@ -9,6 +9,7 @@ class ApiContainer(BaseContainer):
     crypto_service = providers.Factory(CryptoService)
 
     wiring_config = containers.WiringConfiguration(packages=[
+        middlewares,
         shared,
     ])
 
