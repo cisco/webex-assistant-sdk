771f53adbf7bc35f83b71e158d5c37d4981fef1c
Brady Lenz <bradylenz98@gmail.com>
2022-04-13 18:01:25 -0500

    fix: TestInvoker fixes

--- webex_assistant_skills_sdk/shared/services/settings.py
+++ webex_assistant_skills_sdk/shared/services/settings.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from pathlib import Path
 from typing import Optional
 
@@ -7,11 +9,16 @@ from pydantic import BaseSettings
 class Settings(BaseSettings):
     skill_name: Optional[str]
     private_key_path: Path = 'id_rsa.pem'
-    public_key_path: Path = 'id_rsa.pub'
     secret: Optional[str]
     use_encryption: bool = True
     log_level: str = 'INFO'
     app_dir: Optional[str] = None
+    
+    @staticmethod
+    def construct_default_test_settings() -> Settings:
+        return Settings(
+            use_encryption=False,
+        )
 
     class Config:
         env_prefix = 'SKILLS_'
--- webex_assistant_skills_sdk/shared/services/test_invoker.py
+++ webex_assistant_skills_sdk/shared/services/test_invoker.py
@@ -1,9 +1,11 @@
 from __future__ import annotations
 
+from dependency_injector import providers
 from dependency_injector.wiring import Provide
 from fastapi.testclient import TestClient
-from fastapi import Response
+from fastapi import FastAPI, Response
 
+from webex_assistant_skills_sdk.container import container
 from webex_assistant_skills_sdk.shared.models import DeviceContext
 from webex_assistant_skills_sdk.shared.services.base_invoker import BaseInvoker
 from webex_assistant_skills_sdk.shared.services.settings import Settings
@@ -11,18 +13,23 @@ from webex_assistant_skills_sdk.types import Types
 
 
 class TestInvoker(BaseInvoker):
-    _client = TestClient()
+    _client: TestClient
     _settings: Settings =  Provide[Types.SETTINGS]
 
     def __init__(
         self,
+        app: FastAPI,
         device_context: DeviceContext = DeviceContext.construct_default_context(),
+        settings_override: Settings = Settings.construct_default_test_settings(),
     ) -> None:
         super().__init__(
             device_context=device_context,
             use_encryption=False,
             url='/parse',
         )
+
+        container.settings.override(providers.Singleton(settings_override))
+        self._client = TestClient(app=app)
 
     def post_request(self, request_json: str) -> Response:
         return self._client.post(self.url, data=request_json)
